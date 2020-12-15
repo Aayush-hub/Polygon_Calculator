7b092e48e2deb66742d3bf6ec1d77085ddfde1bc
Aayush Garg <65889104+Aayush-hub@users.noreply.github.com>
2020-12-15 21:43:02 +0530

    Add files via upload

---
+++ polygon.py
@@ -0,0 +1,53 @@
+import math
+def square(a):
+    area = a*a
+    print(f'Area is: {area}')
+    perimeter = 4*a
+    print(f'Perimeter is: {perimeter}')  
+def rectangle(a,b):
+    area = a*b
+    print(f'Area is: {area}')
+    perimeter = 2*(a+b)
+    print(f'Perimeter is: {perimeter}')
+def triangle(x,y,z,a,b):
+    area = 0.5*(a*b)
+    print(f'Area is: {area}')
+    perimeter = x+y+z
+    print(f'Perimeter is: {perimeter}')
+def parallelogram(x,y,z,w,a,b):
+    area = a*b
+    print(f'Area is: {area}')
+    perimeter = x+y+z+w
+    print(f'Perimeter is: {perimeter}')
+def circle(a):
+    area = round(math.pi*a*a,3)
+    print(f'Area is: {area}')
+    perimeter = round(2*math.pi*a,3)
+    print(f'Perimeter is: {perimeter}')
+if __name__ == "__main__":
+    n = input("Enter shape: \n").upper()
+    if n == "SQUARE":
+        a = float(input("Enter Side: \n"))
+        square(a)
+    if n == "RECTANGLE":
+        a = float(input("Enter Length: \n"))
+        b = float(input("Enter Breadth: \n"))
+        rectangle(a,b)
+    if n == "TRIANGLE":
+        x = float(input("Enter 1st co-ordinate: \n"))
+        y = float(input("Enter 2nd co-ordinate: \n"))
+        z = float(input("Enter 3rd co-ordinate: \n"))
+        a = float(input("Enter Base: \n"))
+        b = float(input("Enter Height: \n"))
+        triangle(x,y,z,a,b)
+    if n == "PARALLELOGRAM":
+        x = float(input("Enter Length of 1st side: \n"))
+        y = float(input("Enter Length of 2nd side : \n"))
+        z = float(input("Enter Length of 3rd side : \n"))
+        w = float(input("Enter Length of 4th side : \n"))
+        a = float(input("Enter Base: \n"))
+        b = float(input("Enter Height: \n"))
+        parallelogram(x,y,z,w,a,b)
+    if n == "CIRCLE":
+        a = int(input('Enter radius: \n'))
+        circle(a)
