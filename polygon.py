092eae14c3587c8c1bf0c32a6c7cda5e26518b8a
Aayush Garg <65889104+Aayush-hub@users.noreply.github.com>
2020-12-16 12:40:22 +0530

    Update polygon.py

--- polygon.py
+++ polygon.py
@@ -24,6 +24,19 @@ def circle(a):
     print(f'Area is: {area}')
     perimeter = round(2*math.pi*a,3)
     print(f'Perimeter is: {perimeter}')
+def rhombus(a,b):
+    area = a*b
+    print(f'Area is: {area}')
+    perimeter = 4*a
+    print(f'Perimeter is: {perimeter}')
+def trapezoid(x,y,z,w,a,q,b):
+    area = 0.5*((a+q)*b)
+    print(f'Area is: {area}')
+    perimeter = x+y+z+w
+    print(f'Perimeter is: {perimeter}')
+
+
+
 if __name__ == "__main__":
     n = input("Enter shape: \n").upper()
     if n == "SQUARE":
@@ -49,5 +62,18 @@ if __name__ == "__main__":
         b = float(input("Enter Height: \n"))
         parallelogram(x,y,z,w,a,b)
     if n == "CIRCLE":
-        a = int(input('Enter radius: \n'))
+        a = float(input('Enter radius: \n'))
         circle(a)
+    if n == "RHOMBUS":
+        a = float(input("Enter Base: \n"))
+        b = float(input("Enter Height: \n"))
+        rhombus(a,b)
+    if n == "TRAPEZOID":
+        x = float(input("Enter Length of 1st side: \n"))
+        y = float(input("Enter Length of 2nd side : \n"))
+        z = float(input("Enter Length of 3rd side : \n"))
+        w = float(input("Enter Length of 4th side : \n"))
+        a = float(input("Enter Large Base Length : \n"))
+        q = float(input("Enter Small Base Length : \n"))
+        b = float(input("Enter Height: \n"))
+        trapezoid(x,y,z,w,a,q,b)
