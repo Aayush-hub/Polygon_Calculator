68fa1e0cda6d3506a4e3f721f9a02d369ac26c0c
Aayush Garg <65889104+Aayush-hub@users.noreply.github.com>
2021-01-15 16:41:13 +0530

    Update main.py

--- flask/main.py
+++ flask/main.py
@@ -1,15 +1,16 @@
 from flask import Flask , render_template , request
+import math
 app = Flask(__name__)
-# data = {"Square":["Area:"],"Rectangle":["auidchq"],"Triangle":["auidchq"],"Parallelogram":["auidchq"],"Circle":["auidchq"],"Rhombus":["auidchq"],"Trapezoid":["auidchq"]}
 
 @app.route("/")
 def hello():
     return render_template("form.html")
+
 @app.route("/submit/")
 def anwer():
     if request.args.get("shape"):
         global lis
-        lis = request.args.get("shape")
+        lis = request.args.get("shape").lower()
         
         if lis == "square":
             return render_template("form_sq.html")
@@ -17,9 +18,19 @@ def anwer():
             return render_template("form_rec.html")
         if lis == "triangle":
             return render_template("form_tria.html")
+        if lis == "parallelogram":
+            return render_template("form_par.html")
+        if lis == "circle":
+            return render_template("form_ci.html")
+        if lis == "rhombus":
+            return render_template("form_rho.html")
+        if lis == "trapezoid":
+            return render_template("form_tra.html")
+
 
 @app.route("/submit/shape")
 def out():
+    
     if lis == "square":
         
         if request.args.get("side"):
@@ -27,6 +38,7 @@ def out():
             area = a*a
             perimeter = 4*a
             return (f'Area is: {area}, Perimeter is: {perimeter}')
+    
     if lis == "rectangle":
         
         if request.args.get("length") and request.args.get("breadth"):
@@ -35,6 +47,7 @@ def out():
             area = a*b
             perimeter = 2*(a+b)
             return (f'Area is: {area}, Perimeter is: {perimeter}')
+    
     if lis == "triangle":
         if request.args.get("1st co-ordinate") and request.args.get("2nd co-ordinate") and request.args.get("3rd co-ordinate") and request.args.get("Base") and request.args.get("Height"):
             x = int(request.args.get("1st co-ordinate"))
@@ -45,6 +58,44 @@ def out():
             area = 0.5*(a*b)
             perimeter = x+y+z
             return (f'Area is: {area}, Perimeter is: {perimeter}')
-            
-
+    
+    if lis == "parallelogram":
+        if request.args.get("1st side") and request.args.get("2nd side") and request.args.get("3rd side") and request.args.get("4th side") and request.args.get("Base") and request.args.get("Height"):
+            x = int(request.args.get("1st side"))
+            y = int(request.args.get("2nd side"))
+            z = int(request.args.get("3rd side"))
+            w = int(request.args.get("4th side"))
+            a = int(request.args.get("Base"))
+            b = int(request.args.get("Height"))
+            area = a*b
+            perimeter = x+y+z+w
+            return (f'Area is: {area}, Perimeter is: {perimeter}')
+    
+    if lis == "circle":
+        if request.args.get("radius"):
+            a = int(request.args.get("radius"))
+            area = round(math.pi*a*a,3)
+            perimeter = round(2*math.pi*a,3)
+            return (f'Area is: {area}, Perimeter is: {perimeter}')
+    
+    if lis == "rhombus":
+        if request.args.get("base") and request.args.get("height"):
+            a = int(request.args.get("base"))
+            b = int(request.args.get("height"))
+            area = a*b
+            perimeter = 4*a
+            return (f'Area is: {area}, Perimeter is: {perimeter}')
+    
+    if lis == "trapezoid":
+        if request.args.get("1st side") and request.args.get("2nd side") and request.args.get("3rd side") and request.args.get("4th side") and request.args.get("Large Base Length") and request.args.get("Small Base Length") and request.args.get("Height"):
+            x = int(request.args.get("1st side"))
+            y = int(request.args.get("2nd side"))
+            z = int(request.args.get("3rd side"))
+            w = int(request.args.get("4th side"))
+            a = int(request.args.get("Large Base Length"))
+            q = int(request.args.get("Small Base Length"))
+            b = int(request.args.get("Height"))
+            area = 0.5*((a+q)*b)
+            perimeter = x+y+z+w
+            return (f'Area is: {area}, Perimeter is: {perimeter}')
 app.run()
